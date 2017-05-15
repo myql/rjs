4ae94b5d8d03b94bd7bcaf50bcf70b3ecb6e766c
276466206@qq.com <wuqiong@DuoLunTechnology.com>
2017-05-15 14:37:01 +0800

    some

--- music.js
+++ music.js
@@ -68,6 +68,45 @@ function getLyrics(){
 	$.get('http://api.jirengu.com/fm/getLyric.php',{sid:sid},function(lyr){
 		var lyr=JSON.parse(lyr);
 		var line=lyr.lyric.split('\n')
-		
-	})
+		var timeReg=/\[\d{2}:\d{2}.\d{2}\]/g;
+		var result=[];
+		if(line!==''){
+			for(var i in line){
+				var time=line[i].match(timeReg);
+				var value=line[i].replace(timeReg,'');
+				for(j in time){
+					var t=time[j].slice(1,-1).split(':')
+					var timeArr=parseInt(t[0],10)*60+parseFloat(t[1]);
+					result.push([timeArr,value])
+				}
+			}
+		}
+		result.sort(function(a,b){
+			return a[0]-b[0]
+		});
+		lyricArr=result;
+	renderLyric()
+	})	
+}
+function renderLyric(){
+	var lyrLi='';
+	for(var i=0;i<lyricArr.lenght;i++){
+		lyrLI+="<li data-time='"+lyricArr[i][0]+"'+lyricArr[i][1]"+"</li>";
+		$('.music-lyric' ).append(lyrLi);
+		setInterval(showLyric,100);
+	}
+}
+function showLyric(){
+    var liH = $(".music-lyric li").eq(5).outerHeight()-3; 
+    for(var i=0;i< lyricArr.length;i++){
+        var curT = $(".music-lyric li").eq(i).attr("data-time");//获取当前li存入的当前一排歌词时间
+        var nexT = $(".music-lyric li").eq(i+1).attr("data-time");
+        var curTime = myAudio.currentTime;
+        if ((curTime > curT) && (curT < nexT)){//当前时间在下一句时间和歌曲当前时间之间的时候 就渲染 并滚动
+            $(".music-lyric li").removeClass("active");
+            $(".music-lyric li").eq(i).addClass("active");
+            $('.music-lyric').css('top', -liH*(i-2));
+        }
+    }
+
 }
