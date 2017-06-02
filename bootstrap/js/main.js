584df966d9a71ebf825caf8db3d2c0b671c90e6b
276466206@qq.com <wuqiong@DuoLunTechnology.com>
2017-06-02 16:57:32 +0800

    some

--- bootstrap/js/main.js
+++ bootstrap/js/main.js
@@ -1,15 +1,34 @@
 var $navbar=$('.navbar'),$navbarNav=$('.navbar-nav');
-var $container=$('#container'),$services=$('#services'),$portfolio=$('#portfolio'),$about=$('#about'),$team=$('#team'),$contact=$('#contact');
+var $container=$('#container');
+var $navbarBrand=$('.navbar-brand');
+$navbarNav.children().on('click',function(){
+		var idx=$(this).index();
+		$navbarNav.children().removeClass('active').eq(idx).
+		addClass('active');
+	})
 var windowTop=$(window).scrollTop();
-if((windowTop)>2){
+if((windowTop)>10){
 		$navbar.css({
 		background: '#222'
 	})	
+
 	}else {
 		$navbar.css({
 		background: 'none'
-	})	
+		})	
 	}
+	
+
+	for(var i=0;i<$container.children().length-1;i++){
+	var divTop=$container.children().eq(i).offset().top;
+	var divTop1=$container.children().eq(0).offset().top;
+	if(windowTop>divTop){
+		$navbarNav.children().removeClass('active').eq(i).addClass('active');
+		
+	}else if(windowTop<divTop1){
+		$navbarNav.children().removeClass('active')
+	}
+}
 $(window).scroll(function(){
 	var windowTop=$(window).scrollTop();
 	var headerTop=$('.header-content').offset().top;
@@ -22,17 +41,20 @@ $(window).scroll(function(){
 		background: 'none'
 	})	
 	}
-	for(var i=0;i<$container.children().length-1;i++){
+	for(var i=0;i<($container.children().length-1);i++){
 	var divTop=$container.children().eq(i).offset().top;
-	if(windowTop>divTop){
-		$navbarNav.children().eq(i).addClass('active').siblings().removeClass('active');
+	var divTop1=$container.children().eq(0).offset().top;
+	if((windowTop+$(window).height())>divTop){
+		$navbarNav.children().removeClass('active').eq(i).addClass('active');
 		
+	}else if((windowTop+$(window).height())<divTop1) {
+		$navbarNav.children().removeClass('active')
 	}
 	
 }
 })
 
 $navbarNav.children().on('click',function(){
-		$navbarNav.children().removeClass('active');
-		$(this).addClass('active');
-	})
+	
+})
+
